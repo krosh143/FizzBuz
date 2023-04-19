e6b52fa0cc22b7a1c0f59ba0c482b462d42332d1
krosh143 <129940004+krosh143@users.noreply.github.com>
2023-04-19 10:07:00 +0300

    Update script.js

--- FizzBuzz/script.js
+++ FizzBuzz/script.js
@@ -1,16 +1,12 @@
-const code = []
-for (let i = 1; i <= 20; i++){
+const code = (begin,end) => {
+    for (let i = begin; i <= end; i++){
   if (i % 15 === 0) {
-    code.push('FizzBuzz')
-  }
-  else if (i % 3 === 0){
-    code.push('Fizz')
-  }
-  else if (i % 5 === 0){
-    code.push('Buzz')
-  }
-  else{
-    code.push(i)
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.logcode.push('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
   }
 }
-console.log(code)
